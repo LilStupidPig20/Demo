a10145a65f30dd5f983e5188682f369bed2fe8dc
miste <mistersaxar@gmail.com>
2021-06-15 19:24:33 +0500

    кнопки v2

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        Drawable shape = ResourcesCompat.getDrawable(res, R.drawable.button_1_main_list, getTheme());
+
+        TextView tv1 = (TextView)findViewById(R.id.button1_main_list);
+        tv1.setBackground(shape);
     }
 }
