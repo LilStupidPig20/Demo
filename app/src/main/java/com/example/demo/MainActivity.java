7bd7c469562a0c4e18250d427d603f1011559dda
ADZRABARAKURA <alekseyusov51@gmail.com>
2021-06-17 00:16:46 +0500

    update

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv1 = (TextView)findViewById(R.id.button1_main_list);
         tv1.setBackground(shape);
+
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button4_main_list);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCoffee();
+            }
+        });
+
     }
+    public void openActivityCoffee(){
+        Intent intent = new Intent(this, MainCoffeeActivity.class);
+        startActivity(intent);
+    }
+
 }
