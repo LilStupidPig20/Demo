4b72baf47919016a4e20549a9119ae6302140b69
miste <mistersaxar@gmail.com>
2021-06-18 01:57:52 +0500

    popup window v4

---
+++ app/src/main/java/com/example/demo/PopupWindowFromMainMenu.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PopupWindowFromMainMenu extends AppCompatActivity {
+    private Button buttonTheory;
+    private Button buttonTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_window_from_main_menu);
+
+        buttonTheory = (Button) findViewById(R.id.theory_popup);
+        buttonTheory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCoffeeMachine();
+            }
+        });
+
+        buttonTest = (Button) findViewById(R.id.test_popup);
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCoffeeMachine();
+            }
+        });
+    }
+
+    public void openActivityCoffeeMachine(){
+        Intent intent = new Intent(this, MainCoffeeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityMachineTest(){
+        Intent intent = new Intent(this, Activity_Machine_Test.class);
+        startActivity(intent);
+    }
+}
