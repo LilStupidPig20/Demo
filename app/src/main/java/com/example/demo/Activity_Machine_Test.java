4d0e7675d3d971e39cc2f09d5e31ebfe3e3fc9bc
ADZRABARAKURA <alekseyusov51@gmail.com>
2021-06-19 00:50:44 +0500

    работающие radiobutton(можно пока только тыкать)

--- app/src/main/java/com/example/demo/Activity_Machine_Test.java
+++ app/src/main/java/com/example/demo/Activity_Machine_Test.java
@@ -3,12 +3,20 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Activity_Machine_Test extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_test);
+        //radioGroup = findViewById(R.id.Rad);
+        //textView = findViewById(R.id.text)
     }
 }
