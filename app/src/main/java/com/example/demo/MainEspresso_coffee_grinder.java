fdb1dd932e143485779778cbbbc18b26c4b5957a
ADZRABARAKURA <alekseyusov51@gmail.com>
2021-06-20 21:43:14 +0500

    часть popup

---
+++ app/src/main/java/com/example/demo/MainEspresso_coffee_grinder.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainEspresso_coffee_grinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_popup_espresso_coffee_grinder);
+    }
+}
